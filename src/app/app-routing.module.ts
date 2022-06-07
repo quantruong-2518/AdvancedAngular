ee570a787640fd46af3bcdc58e78513def41939e
SteveTruong <quantruong2518.dev@gmail.com>
2022-06-07 15:48:50 +0700

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./screens/homepage/homepage.module').then(m => m.HomepageModule),
+  },
+  {
+    path: 'componentA',
+    loadChildren: () =>
+      import('./screens/component-a/component-a.module').then(
+        m => m.ComponentAModule,
+      ),
+  },
+  {
+    path: 'componentB',
+    loadChildren: () =>
+      import('./screens/component-b/component-b.module').then(
+        m => m.ComponentBModule,
+      ),
+  },
+  {
+    path: '**',
+    loadChildren: () =>
+      import('./screens/not-found/not-found.module').then(
+        m => m.NotFoundModule,
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

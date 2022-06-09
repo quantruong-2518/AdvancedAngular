20a24b94055b2db90f6ce6c0607221e21b097987
quana2k49 <truongbaquan2501@gmail.com>
2022-06-10 00:22:57 +0700

    add CVA

--- src/app/screens/component-a/component-a.component.ts
+++ src/app/screens/component-a/component-a.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-component-a',
   templateUrl: './component-a.component.html',
-  styleUrls: ['./component-a.component.scss']
+  styleUrls: ['./component-a.component.scss'],
 })
 export class ComponentAComponent implements OnInit {
+  customForm!: FormGroup;
 
-  constructor() { }
+  constructor(private pb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.customForm = this.pb.group({
+      name: new FormControl(''),
+      age: new FormControl(20),
+    });
   }
-
 }
--- src/app/screens/component-a/component-a.module.ts
+++ src/app/screens/component-a/component-a.module.ts
@@ -3,9 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { ComponentARoutingModule } from './component-a-routing.module';
 import { ComponentAComponent } from './component-a.component';
+import { Child1Module } from 'src/app/components/child1/child1.module';
+import { Child2Module } from 'src/app/components/child2/child2.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ComponentAComponent],
-  imports: [CommonModule, ComponentARoutingModule],
+  imports: [
+    CommonModule,
+    ComponentARoutingModule,
+    Child1Module,
+    Child2Module,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
 export class ComponentAModule {}
